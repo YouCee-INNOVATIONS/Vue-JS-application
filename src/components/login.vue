0967397b5957b2db2ec441b3f93f925253c67722
YouCee <64170266+YouCee-INNOVATIONS@users.noreply.github.com>
2023-02-28 21:11:41 +0000

    added login form

---
+++ src/components/login.vue
@@ -0,0 +1,34 @@
+<template>
+    <Modal>
+      <ModalForm :title="Login" :excerpt="Welcome back again!">
+        <template #form>
+            <form>
+                <input type="email" name="" v-model="email" id="">
+                <input type="password" v-model="password" />
+                <button>Submit</button>
+            </form>
+        </template>
+
+      </ModalForm>
+    </Modal>
+    
+    
+
+</template>
+<script>
+    import ModalForm from './ModalForm.vue';
+    import Modal from './Modal.vue';
+    export default {
+        components: {
+            ModalForm,
+            Modal
+        },
+        data() {
+            return {
+                email: "",
+                password: "",
+            }
+        }
+    }
+
+</script>
